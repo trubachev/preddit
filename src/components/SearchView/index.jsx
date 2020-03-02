67648fd8d682b305c02f8d2ee3270f598efc7105
trubachev <trubachev.artem@gmail.com>
2020-03-02 23:56:43 +0100

    feat(search form): Use Button component

--- src/components/SearchView/index.jsx
+++ src/components/SearchView/index.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import { Link } from "wouter"
 
 import { urls } from "~/routes"
+import Button from "~/components/Button"
 
 import css from "./styles.scss"
 
@@ -22,7 +23,7 @@ const SearchView = ({ suggestions, onSubmitSearch, onChangeSearch }) => {
             placeholder="subreddit name"
             onChange={handleChangeSearch}
           />
-          <button>Go</button>
+          <Button>Go</Button>
         </div>
       </form>
 
